a119ed96af42e815d59eb15c058aa0c467b02a8e
vladimir-dresevic <vladimir.dresevic@gmail.com>
2026-02-25 17:04:30 +0100

    first ETL pipeline completed

--- first_etl_pipeline/first_etl_pipeline.py
+++ first_etl_pipeline/first_etl_pipeline.py
@@ -1,19 +1,49 @@
 import pandas as pd
 
-df = pd.read_excel("users_rentals_v2.xlsx", engine="openpyxl")
-
-for i in range(df.shape[0]):
-    value = df.loc[i, "rental_date"]
-
-    if(type(value).__name__ != "datetime" and not(pd.isna(value))):
-
-        df.loc[i, "rental_date"] = pd.to_datetime(value, format="%d.%m.%Y", errors="raise")
-
-df["rental_date"] = pd.to_datetime(df["rental_date"], errors="raise")
+#extract
+def extract_data(file_name):
+    return pd.read_excel(file_name, engine="openpyxl")
 
 
-mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
-new_df = df[mask]
+#clean
+def clean_data(df):
+    for i in range(df.shape[0]):
+        value = df.loc[i, "rental_date"]
 
-print(new_df)
+        if(type(value).__name__ != "datetime" and not(pd.isna(value))):
 
+            df.loc[i, "rental_date"] = pd.to_datetime(value, format="%d.%m.%Y", errors="raise")
+
+    df["rental_date"] = pd.to_datetime(df["rental_date"], errors="raise")
+    return df
+
+
+#filter data
+def filter_data(df):
+    mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
+    return df[mask]
+
+
+#sort data
+def sort_data(df):
+    return df.sort_values(by='rental_date', ascending=True)
+
+
+# add new column
+def add_new_column(df):
+    df['overdue_days'] = (pd.Timestamp.today() - df['rental_date']).dt.days - 31
+    return df
+
+
+#remove columns
+def remove_columns(df):
+    return df.drop(columns=['address', 'gender', 'city', 'active'])
+
+
+# load data
+def load_data(df):
+    df.to_excel("overdue_users.xlsx", index=False)
+    return df
+
+
+df = extract_data('users_rentals_v2.xlsx').pipe(clean_data).pipe(filter_data).pipe(sort_data).pipe(add_new_column).pipe(remove_columns).pipe(load_data)
